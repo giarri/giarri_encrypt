b5b949c495b3a840ac006ff49f059db0e40e1d46
Giovanni Arriciati <100694500+giarri@users.noreply.github.com>
2026-06-08 20:52:26 +0200

    more windows problems
    Signed-off-by: Giovanni Arriciati <100694500+giarri@users.noreply.github.com>

--- include/logging.hpp
+++ include/logging.hpp
@@ -1,11 +1,14 @@
 //
 // Created by Giovanni Arriciati on 07/06/26.
 //
+#pragma once
 
-#ifndef GIARRI_ENCRYPT_LOGGING_H
-#define GIARRI_ENCRYPT_LOGGING_H
+#ifndef __FILE_NAME__
+    #define __FILE_NAME__ __FILE__
+#endif
+
+
 #define LOG(msg) \
     std::clog << __FILE_NAME__ << "(" << __LINE__ << "): " << msg << std::endl;
 #define LOG_ERR(msg) \
     std::cerr << "ERROR\t" << __FILE_NAME__ << "(" << __LINE__ << "): " << msg << std::endl;
-#endif //GIARRI_ENCRYPT_LOGGING_H
